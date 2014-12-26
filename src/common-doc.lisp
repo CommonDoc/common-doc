19573b8853d867ae5713a322eb9cd79a0ced269f
Fernando Borretti <eudoxiahp@gmail.com>
2014-12-26 11:21:38 -0200

    Export classes, slots and methods

--- src/common-doc.lisp
+++ src/common-doc.lisp
@@ -1,7 +1,74 @@
 (in-package :cl-user)
 (defpackage common-doc
-  (:use :cl :trivial-types))
+  (:use :cl :trivial-types)
+  (:export :<document-node>
+           :<content-node>
+           :<text-node>
+           :<paragraph>
+           :<markup>
+           :<bold>
+           :<italic>
+           :<underline>
+           :<strikethrough>
+           :<code>
+           :<superscript>
+           :<subscript>
+           :<code-block>
+           :<verbatim>
+           :<quote>
+           :<inline-quote>
+           :<block-quote>
+           :<link>
+           :<internal-link>
+           :<external-link>
+           :<web-link>
+           :<list>
+           :<list-item>
+           :<definition>
+           :<unordered-list>
+           :<ordered-list>
+           :<definition-list>
+           :<image>
+           :<figure>
+           :<table>
+           :<row>
+           :<cell>
+           :<section>
+           :<document>
+           :metadata
+           :children
+           :text
+           :language
+           :section-reference
+           :document-reference
+           :uri
+           :term
+           :definition
+           :items
+           :source
+           :description
+           :rows
+           :header
+           :footer
+           :cells
+           :title
+           :reference
+           :content
+           :title
+           :creator
+           :publisher
+           :subject
+           :description
+           :keywords
+           :rights
+           :version
+           :created-on
+           :traverse-document
+           :collect-figures
+           :collect-tables))
 (in-package :common-doc)
+
+
 
 ;;; Basic classes
 
