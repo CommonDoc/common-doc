3c532517056fe18b18359078bc4f7d5a7194a2b8
Fernando Borretti <eudoxiahp@gmail.com>
2014-12-26 09:41:20 -0200

    Add a generic function to traverse documents

--- src/common-doc.lisp
+++ src/common-doc.lisp
@@ -244,3 +244,22 @@
                created. By default, this is the date and time at instance
                creation."))
   (:documentation "A document."))
+
+;;; Code
+
+(defgeneric traverse-document (node function)
+  (:documentation "Apply a side effectful function recursively to every element
+  in the document. Depth-first.")
+
+  (:method ((doc <document>) function)
+    (funcall function doc)
+    (loop for child in (content doc) do
+      (traverse-document child function)))
+
+  (:method ((cnode <content-node>) function)
+    (funcall function cnode)
+    (loop for child in cnode do
+      (traverse-document child function)))
+
+  (:method ((dnode <document-node>) function)
+    (funcall function dnode)))
