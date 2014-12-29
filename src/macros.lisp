8ae48f5fe196360524aa437421cccd76af937b64
Fernando Borretti <eudoxiahp@gmail.com>
2014-12-29 21:08:04 -0200

    Add macros.lisp

---
+++ src/macros.lisp
@@ -0,0 +1,12 @@
+(in-package :common-doc)
+
+(defclass <macro-node> (<content-node>)
+  ((name :accessor name
+         :initarg :name
+         :type string
+         :documentation "The name of the macro.")
+   (attributes :accessor attributes
+               :initarg :attributes
+               :type hash-table
+               :documentation "Macro attributes."))
+  (:documentation "A macro to be expanded."))
