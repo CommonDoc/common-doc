664fe6b9ebbdbddc990219915b0fb7ab1c6ac154
Fernando Borretti <eudoxiahp@gmail.com>
2015-01-03 16:28:12 -0200

    A more complex define-node

--- src/define.lisp
+++ src/define.lisp
@@ -1,20 +1,47 @@
 (in-package :common-doc)
 
-(defparameter *registry* (make-hash-table :test #'equal))
+(defparameter *registry* (make-hash-table :test #'equal)
+  "A hash table from tag names to node classes.")
+
+(defparameter *node-slots* (make-hash-table :test #'equal))
 
 (defmacro define-node (name (&rest superclasses) slots &rest class-options)
+  "Define a CommonDoc node."
   (flet ((tag-name-p (opt)
+           "Whether `opt` is a list of the form `(:tag-name ...)`."
            (and (listp opt)
-                (eq (first opt) :tag-name))))
-    (let ((tag-name (find-if #'tag-name-p class-options))
-          (class-options (remove-if #'tag-name-p class-options)))
+                (eq (first opt) :tag-name)))
+         (extract-slot-names (slots)
+           "Extract the value of the `:attribute-name` key of a slot, if it exists."
+           (let ((final-slots (list))
+                 (special-slots (list)))
+             ;; `final-slots` is the list of slots that will be handed to the
+             ;; `defclass` form, while `special-slots` is an alist of attribute
+             ;; names to slot names
+             (loop for slot in slots do
+               (let ((slot-attrs (rest slot)))
+                 (awhen (getf slot-attrs :attribute-name)
+                   (push (cons it (first slot)) special-slots))
+                 (push (cons (first slot)
+                             (alexandria:remove-from-plist slot-attrs :attribute-name))
+                       final-slots)))
+             (cons (reverse final-slots)
+                   (reverse special-slots)))))
+    (let* ((tag-name (find-if #'tag-name-p class-options))
+           (class-options (remove-if #'tag-name-p class-options))
+           (slots-and-special-slots (extract-slot-names slots))
+           (slots (first slots-and-special-slots))
+           (special-slots (rest slots-and-special-slots)))
       `(progn
          (defclass ,name ,superclasses
            ,slots
            ,@class-options)
          ,(if tag-name
-              `(setf (gethash ,(cadr tag-name) *registry*)
-                     (find-class ',name)))
+              `(progn
+                 (setf (gethash ,(cadr tag-name) *registry*)
+                       (find-class ',name))
+                 (setf (gethash ,(cadr tag-name) *node-slots*)
+                       ,special-slots)))
          t))))
 
 (defun find-node (tag-name)
