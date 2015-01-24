c85f52152263b2486112defd6d8160d94e2cb9d5
Fernando Borretti <eudoxiahp@gmail.com>
2015-01-24 12:30:05 -0200

    Add excise-paragraph-separators function

--- contrib/split-paragraphs/split-paragraphs.lisp
+++ contrib/split-paragraphs/split-paragraphs.lisp
@@ -23,3 +23,24 @@ string if it has none."
   (if (has-paragraph-separator string)
       (ppcre:split "\\n\\n" string)
       string))
+
+(defun excise-paragraph-separators (list)
+  "Take a list of strings or other elements. Separate strings by paragraphs,
+leaving a paragraph marker between each string."
+  (let ((output (list)))
+    (loop for elem in list do
+      (if (stringp elem)
+          ;; If it's a string, split it by its separator
+          (let ((split (split-paragraph elem)))
+            (if (stringp split)
+                ;; Just a regular string with no separators, add it to the
+                ;; output
+                (push split output)
+                ;; A list of text nodes, add each to the output, followed by a
+                ;; paragraph separator marker
+                (loop for text in split do
+                  (push text output)
+                  (push +paragraph-marker+ output))))
+          ;; If it's another node, add it to the output unconditionally
+          (push elem output)))
+    output))
