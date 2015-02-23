c0a859ec8371378d331218726bc3e9b79703847c
Fernando Borretti <eudoxiahp@gmail.com>
2015-02-23 18:43:10 -0200

    Better text to slug

--- src/util.lisp
+++ src/util.lisp
@@ -40,5 +40,6 @@
   "Take a string, usually the name of a section, and create something that is
 more similar to an identifier, i.e. no spaces, same case, etc."
   (let* ((no-space (substitute #\- #\Space string))
-         (no-slashes (substitute #\- #\/ no-space)))
-    (string-downcase no-slashes)))
+         (no-slashes (substitute #\- #\/ no-space))
+         (no-colons (substitute #\- #\: no-slashes)))
+    (string-downcase no-colons)))
