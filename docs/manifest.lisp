594fecd6a3468413b488a246f260ce0ba2e83aaa
Fernando Borretti <eudoxiahp@gmail.com>
2015-02-28 15:54:59 -0200

    Add docs manifest

---
+++ docs/manifest.lisp
@@ -0,0 +1,10 @@
+(:markup-format :vertex
+ :systems (:common-doc)
+ :documents ((:title "CommonDoc"
+              :authors ("Fernando Borretti")
+              :markup-format :vertex
+              :output-format (:type :multi-html
+                              :template :min)
+              :sources ("nodes.tex"
+                        "defining-nodes.tex"
+                        "util.tex"))))
