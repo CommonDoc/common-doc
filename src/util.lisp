c8918286e5253638a8974048bc3bedcfbcc2ce30
Fernando Borretti <eudoxiahp@gmail.com>
2015-05-13 18:11:16 -0300

    Remove most utils, which are no longer necessary

--- src/util.lisp
+++ src/util.lisp
@@ -1,53 +1,5 @@
 (in-package :common-doc.util)
 
-(defmacro doc (class args &rest children)
-  "Easily create a document or node.
-
-\\code[lang=lisp]{
-(doc subscript ())
-}
-
-is equivalent to:
-
-\\code[lang=lisp]{
-(make-instance 'subscript)
-}
-
-\\code[lang=lisp]{
-(doc document
-     (:title \"My Document\")
-     (text-node (:text \"...\")))
-}
-
-is equivalent to:
-
-\\code[lang=lisp]{
-(make-instance 'document
-               :title \"My Document\"
-               :children (list (make-instance 'text-node :text \"...\")))
-}"
-  (labels ((recur (args)
-             (destructuring-bind (class args &rest children) args
-               (if children
-                   `(make-instance ',class
-                                   ,@args
-                                   :children (list
-                                              ,@(loop for child in children collecting
-                                                  (recur child))))
-                   `(make-instance ',class ,@args)))))
-    (recur (cons class (cons args children)))))
-
-(defun make-meta (pairs)
-  "Create a metadata hash table from a list of cons cells."
-  (let ((table (make-hash-table :test #'equal)))
-    (loop for pair in pairs do
-      (setf (gethash (first pair) table) (rest pair)))
-    table))
-
-(defun make-text (string &optional metadata)
-  "Create a text node from the contents of a string."
-  (make-instance 'text-node :text string :metadata metadata))
-
 (defun string-to-slug (string)
   "Take a string, usually the name of a section, and create something that is
 more similar to an identifier, i.e. no spaces, same case, etc."
