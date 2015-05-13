73aaa8bb2643db9fc33754fbdc044b18834d763f
Fernando Borretti <eudoxiahp@gmail.com>
2015-05-13 18:55:18 -0300

    Add constructor tests

--- t/common-doc.lisp
+++ t/common-doc.lisp
@@ -8,6 +8,39 @@
   :description "common-doc tests.")
 (in-suite basic-tests)
 
+(test constructors
+  (is-true
+   (typep (make-strikethrough nil) 'strikethrough))
+  (is-true
+   (typep (make-superscript nil) 'superscript))
+  (is-true
+   (typep (make-subscript nil) 'subscript))
+  (let ((node (make-code-block "lisp" nil)))
+    (typep node 'code-block)
+    (is (equal (language node) "lisp")))
+  (is-true
+   (typep (make-inline-quote nil) 'inline-quote))
+  (is-true
+   (typep (make-block-quote nil) 'block-quote))
+  (is-true
+   (typep (make-document-link "doc" "sec" nil)
+          'document-link))
+  (is-true
+   (typep (make-web-link "http://www.example.com" nil)
+          'web-link))
+  (is-true
+   (typep (make-list-item nil) 'list-item))
+  (is-true
+   (typep (make-unordered-list nil) 'unordered-list))
+  (is-true
+   (typep (make-ordered-list nil) 'ordered-list))
+  (let ((def (make-definition nil nil)))
+    (is-true
+     (typep def 'definition))
+    (is-true (make-definition-list (list def))
+             'definition-list)))
+
+
 (test simple-doc
   (let ((document (make-document
                    "My Document"
