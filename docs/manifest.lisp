9e250ae5e9e34656c85621374b46f35b18c8fee2
Fernando Borretti <eudoxiahp@gmail.com>
2015-05-28 22:44:16 -0300

    Update manifest

--- docs/manifest.lisp
+++ docs/manifest.lisp
@@ -3,13 +3,13 @@
  :documents ((:title "CommonDoc"
               :authors ("Fernando Borretti")
               :output-format (:type :multi-html
-                              :template :min)
+                              :template :minima)
               :sources ("overview.scr"
                         "formats.scr"
                         "nodes.scr"
                         "defining-nodes.scr"
                         "files.scr"
                         "errors.scr"
-                        "macros.scr"
+                        ;"macros.scr"
                         "libraries.scr"
                         "extensions.scr"))))
