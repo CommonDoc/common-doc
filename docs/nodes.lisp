01aaba4c8f8ddaa6381b20cf0f7a8e37c9d87400
Fernando Borretti <eudoxiahp@gmail.com>
2015-05-31 12:40:17 -0300

    Move node example stuff to separate file, add an example

---
+++ docs/nodes.lisp
@@ -0,0 +1,31 @@
+(in-package :common-doc)
+
+(make-document "My Document"
+  :children
+  (list
+    (make-section (list (make-text "Introduction"))
+      :children
+      (list
+        (make-paragraph
+          (list (make-text "...")))))))
+
+COMMON-DOC> (dump my-doc)
+document
+  section
+    paragraph
+      text-node
+        "..."
+
+CL-USER> (in-package :common-doc)
+#<PACKAGE "COMMON-DOC">
+
+COMMON-DOC> (text (make-text "Hello, world!"))
+"Hello, world!"
+
+COMMON-DOC> (children (make-paragraph
+                        (list (make-text "This is ")
+                              (make-text "a test"))))
+(#<TEXT-NODE text: This is > #<TEXT-NODE text: a test>)
+
+COMMON-DOC> (children (make-paragraph (list (make-text "This is ") (make-text "a test"))))
+(#<TEXT-NODE text: This is > #<TEXT-NODE text: a test>)
