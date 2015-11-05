60e2ae1e4cfe3ba7d7ce5a666653c746cabfd8f4
Fernando Borretti <eudoxiahp@gmail.com>
2015-11-04 21:56:01 -0300

    setfable metadata

--- src/metadata.lisp
+++ src/metadata.lisp
@@ -12,7 +12,16 @@ empty metadata table."
 (defun get-meta (node key)
   "Find the value corresponding to @cl:param(key) in the node's metadata. If not
 found, return @c(NIL)."
-  (gethash key (metadata node)))
+  (when (metadata node)
+    (gethash key (metadata node))))
+
+(defun (setf get-meta) (value node key)
+  (if (metadata node)
+      (setf (gethash key (metadata node)) value)
+      (progn
+        (setf (metadata node)
+              (make-hash-table :test #'equal))
+        (setf (get-meta node key) value))))
 
 (defmacro do-meta ((key value node) &body body)
   "Iterate over the keys and values of a node's metadata."
